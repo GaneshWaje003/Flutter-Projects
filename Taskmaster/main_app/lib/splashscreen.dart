56b13cf8cbdddb2ca518923cca290dcbc6e4f87a
Ganesh <gsw7517@gmail.com>
2024-06-20 08:41:59 +0530

    at splash screen

--- Taskmaster/main_app/lib/splashscreen.dart
+++ Taskmaster/main_app/lib/splashscreen.dart
@@ -1,8 +1,36 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:lottie/lottie.dart';
 
-class splashscreen extends StatelessWidget {  @override
+class splashscreen extends StatelessWidget {
+  @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      body: Container(
+        decoration: BoxDecoration(
+          image: DecorationImage(
+            image: AssetImage('assets/main_bg.png'),
+            fit: BoxFit.cover,
+          ),
+        ),
+        child: Column(
+          children: <Widget>[ 
+          
+            Lottie.asset(
+              'animations/taski.json'
+            ,height: 300,
+              width: 300,
+              repeat: true,
+              fit:BoxFit.cover),
+
+
+            Container(
+              child: Image.asset('assets/light_logo.png'),
+            )
+
+          ],
+        ),
+      ),
+    );
   }
 }
